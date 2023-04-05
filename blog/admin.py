30461612f01ba199cda5f32aac84968d36fc54ff
Carvu94 <cmatej08@gmail.com>
2023-04-05 20:33:05 +0000

    Add fields for Category in admin panel

--- blog/admin.py
+++ blog/admin.py
@@ -1,9 +1,21 @@
 from django.contrib import admin
-from .models import Recipe
+from .models import Recipe, Category
 from django_summernote.admin import SummernoteModelAdmin
 
 
 @admin.register(Recipe)
 class RecipeAdmin(SummernoteModelAdmin):
+    """
+    Add fields in admin panel using summernote editor
+    """
 
     summernote_fields = ('content')
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    """
+    Add fields for Category in admin panel
+    """
+    list_display = ['title']
+    search_fields = ['title']
