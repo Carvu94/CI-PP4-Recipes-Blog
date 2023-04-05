408f7a11b27fe47fc1b52f3ba8ba67e70d35929a
Carvu94 <cmatej08@gmail.com>
2023-04-05 21:03:50 +0000

    Edit RecipeAdmin class

--- blog/admin.py
+++ blog/admin.py
@@ -8,7 +8,10 @@ class RecipeAdmin(SummernoteModelAdmin):
     """
     Add fields in admin panel using summernote editor
     """
-
+    list_display = ('title', 'slug', 'status', 'created_on')
+    search_fields = ['title', 'content']
+    prepopulated_fields = {'slug': ('title',)}
+    list_filter = ('status', 'created_on')
     summernote_fields = ('content')
 
 
