5f3292bfaa216e81d42a01f50fcf467bc928c0e4
Matej <macbookpro@apples-MacBook-Pro.local>
2023-04-15 20:14:24 +0100

    edit settings and gitignore to setup vs code

--- hungrychef/settings.py
+++ hungrychef/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['hungry-chef.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['hungry-chef.herokuapp.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
