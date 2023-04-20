1fc5972aa5b80a617112c203da83c36f0978ca60
Carvu94 <cmatej08@gmail.com>
2023-04-21 00:06:20 +0100

    remove email from comment model and admin.py

--- blog/admin.py
+++ blog/admin.py
@@ -35,7 +35,7 @@ class CommentAdmin(admin.ModelAdmin):
     """
     list_display = ('name', 'body', 'post', 'created_on', 'approved')
     list_filter = ('approved', 'created_on')
-    search_fields = ['name', 'email', 'body']
+    search_fields = ['name', 'body']
     actions = ['approve_comments']
 
     def approve_comments(self, request, queryset):
