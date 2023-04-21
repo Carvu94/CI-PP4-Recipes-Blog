3c47aba47fc50a8310aead99bdc1b0e4850c344a
Carvu94 <cmatej08@gmail.com>
2023-04-21 05:46:41 +0100

    deployment commit

--- hungrychef/settings.py
+++ hungrychef/settings.py
@@ -31,6 +31,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
+X_FRAME_OPTIONS = 'SAMEORIGIN'
+
 ALLOWED_HOSTS = ['hungry-chef.herokuapp.com', 'localhost', '127.0.0.1']
 
 
